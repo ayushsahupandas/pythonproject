d56e541e2c49fbb848955e5ef1efd64fafe68ec2
Ayush Sahu <ayushsahu654788@gmail.com>
2026-04-22 22:41:10 +0530

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,15 @@
+# Q1: Demonstrate int, float and type conversion
+
+
+num1 = 10          # int
+num2 = 5.5         # float
+
+print("Integer value:", num1, "Type:", type(num1))
+print("Float value:", num2, "Type:", type(num2))
+
+
+converted_float = float(num1)
+converted_int = int(num2)
+
+print("Converted int to float:", converted_float, "Type:", type(converted_float))
+print("Converted float to int:", converted_int, "Type:", type(converted_int))
