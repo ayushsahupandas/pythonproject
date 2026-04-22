c8f10453f72da2e97f1f36c07ebc9bbe56bf8d36
Ayush Sahu <ayushsahu654788@gmail.com>
2026-04-23 00:00:01 +0530

    Add files via upload

---
+++ Q11.py
@@ -0,0 +1,12 @@
+s = input("Enter a string: ")
+freq = {}
+
+for ch in s:
+    if ch in freq:
+        freq[ch] += 1
+    else:
+        freq[ch] = 1
+
+print("Character Frequency:")
+for key, value in freq.items():
+    print(key, ":", value)
