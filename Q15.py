5585b0fead7d79ed672fb304e5a1da5d1cd7d797
Ayush Sahu <ayushsahu654788@gmail.com>
2026-04-23 00:01:50 +0530

    Add files via upload

---
+++ Q15.py
@@ -0,0 +1,12 @@
+lst = [5, 2, 9, 1]
+print("Length:", len(lst))
+print("Max:", max(lst))
+print("Min:", min(lst))
+print("Sorted:", sorted(lst))
+
+s = "python"
+print("Upper:", s.upper())
+
+d = {"a": 1, "b": 2}
+print("Keys:", d.keys())
+print("Values:", d.values())
